ee59fd93851a7d2e7537b9a00154cd3654d97885
kranjanford <kranjan@ford.com>
2018-08-16 15:44:14 +0530

    Hockeyapp integration

--- openXCenabler/AppDelegate.swift
+++ openXCenabler/AppDelegate.swift
@@ -7,15 +7,17 @@
 //
 
 import UIKit
+import HockeySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    self.setUpHockeySDK()
+   
     return true
   }
 
@@ -35,12 +37,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationDidBecomeActive(_ application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   
   }
 
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
   }
+  func setUpHockeySDK() {
+    
+        BITHockeyManager.shared().configure(withIdentifier: "8684d9e9ea0b4c3bbc9484ccb16965df")
+        //enable crash reporting
+        BITHockeyManager.shared().isCrashManagerDisabled = false
+        BITHockeyManager.shared().crashManager.crashManagerStatus = BITCrashManagerStatus.alwaysAsk
+        BITHockeyManager.shared().start()
+        //This line is obsolete in the crash only builds
+        BITHockeyManager.shared().authenticator.authenticateInstallation()
 
+    }
 
 }
 
